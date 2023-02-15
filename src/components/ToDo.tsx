bf62baf84eb094dba20467064303af02e3eb54f9
Seunghyeok Lee <seunghyeok.tech@gmail.com>
2023-02-16 05:03:58 +0900

    Release 1.0.0 (#4)
    * recap
    
    * Add To Do
    
    * Refactoring
    
    * Categories
    
    * Immutability part One
    
    * feat(ToDo): onClick 함수 안의 setToDos 기능 추가
    
    배열안에 새로운 카데고리 값 변경
    
    See also: NomadCoder react-master 6.14
    
    * feat(ToDoList): toDoState category 분류 기능 추가
    
    Selectors 사용
    
    See also: Nomadcoder react-master 6.16
    
    * 4.17 Selectors part Two
    
    * Enums
    
    * local Storage Done:) (#2)
    
    * Custom category (#3)
    
    * 카테고리 구현
    
    * version 1.0.0
    
    * Update README.md

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,40 +1,35 @@
-import { useSetRecoilState } from "recoil";
-import { Categories, IToDo, toDoState } from "../atoms";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import { categoriesState, IToDo, toDoState } from "../atoms";
 
 function ToDo({ text, category, id }: IToDo) {
   const setToDos = useSetRecoilState(toDoState);
+  const categories = useRecoilValue(categoriesState);
   const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     const {
       currentTarget: { name },
     } = event;
     setToDos(oldToDos => {
       const targertIndex = oldToDos.findIndex(toDo => toDo.id === id);
-      const newToDo = { text, id, category: name as any };
-      return [
+      const newToDo = { text, id, category: name };
+      const newToDos = [
         ...oldToDos.slice(0, targertIndex),
         newToDo,
         ...oldToDos.slice(targertIndex + 1),
       ];
+      localStorage.setItem("toDos", JSON.stringify(newToDos));
+      return newToDos;
     });
   };
   return (
     <li>
       <span>{text}</span>
-      {category !== Categories.DOING && (
-        <button name={Categories.DOING} onClick={onClick}>
-          Doing
-        </button>
-      )}
-      {category !== Categories.TO_DO && (
-        <button name={Categories.TO_DO} onClick={onClick}>
-          To Do
-        </button>
-      )}
-      {category !== Categories.DONE && (
-        <button name={Categories.DONE} onClick={onClick}>
-          Done
-        </button>
-      )}
+      {categories
+        .filter(item => item !== category)
+        .map((v, index) => (
+          <button key={index} name={v} onClick={onClick}>
+            {v}
+          </button>
+        ))}
     </li>
   );
 }
