5b3017e28fbbacc43fef8777269a439ec5ae699b
Seunghyeok Lee <seunghyeok.tech@gmail.com>
2023-02-16 02:09:39 +0900

    Develop (#1)
    * recap
    
    * Add To Do
    
    * Refactoring
    
    * Categories
    
    * Immutability part One
    
    * feat(ToDo): onClick 함수 안의 setToDos 기능 추가
    
    배열안에 새로운 카데고리 값 변경
    
    See also: NomadCoder react-master 6.14
    
    * feat(ToDoList): toDoState category 분류 기능 추가
    
    Selectors 사용
    
    See also: Nomadcoder react-master 6.16
    
    * 4.17 Selectors part Two
    
    * Enums

---
+++ src/components/ToDo.tsx
@@ -0,0 +1,42 @@
+import { useSetRecoilState } from "recoil";
+import { Categories, IToDo, toDoState } from "../atoms";
+
+function ToDo({ text, category, id }: IToDo) {
+  const setToDos = useSetRecoilState(toDoState);
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name },
+    } = event;
+    setToDos(oldToDos => {
+      const targertIndex = oldToDos.findIndex(toDo => toDo.id === id);
+      const newToDo = { text, id, category: name as any };
+      return [
+        ...oldToDos.slice(0, targertIndex),
+        newToDo,
+        ...oldToDos.slice(targertIndex + 1),
+      ];
+    });
+  };
+  return (
+    <li>
+      <span>{text}</span>
+      {category !== Categories.DOING && (
+        <button name={Categories.DOING} onClick={onClick}>
+          Doing
+        </button>
+      )}
+      {category !== Categories.TO_DO && (
+        <button name={Categories.TO_DO} onClick={onClick}>
+          To Do
+        </button>
+      )}
+      {category !== Categories.DONE && (
+        <button name={Categories.DONE} onClick={onClick}>
+          Done
+        </button>
+      )}
+    </li>
+  );
+}
+
+export default ToDo;
